62133add571c9a3864f1174f35aaecf7bffd02a3
Julia <martemyanova.julia@gmail.com>
2016-07-31 02:31:43 +0100

    Add existing sources

---
+++ AndroidBillingV3/src/main/java/synergy/android/billing/reactive/Billing.java
@@ -0,0 +1,102 @@
+package synergy.android.billing.reactive;
+
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.annotation.VisibleForTesting;
+import android.support.v4.app.FragmentActivity;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+
+import rx.Observable;
+import rx.subjects.BehaviorSubject;
+import synergy.android.billing.v3.util.IabException;
+import synergy.android.billing.v3.util.IabHelper;
+import synergy.android.billing.v3.util.IabResult;
+import synergy.android.billing.v3.util.Inventory;
+
+public class Billing {
+
+	private static final int REQUEST_PURCHASE = 12063;
+
+	private final BehaviorSubject<Inventory> inventorySubject = BehaviorSubject.create();
+	private final IabHelper iabHelper;
+	private final ExecutorService background;
+	private Future<?> lastInventoryJob;
+	private boolean initialized;
+
+	public Billing(IabHelper iabHelper, ExecutorService backgroundExecutor) {
+		this.iabHelper = iabHelper;
+		this.background = backgroundExecutor;
+		iabHelper.startSetup(this::onSetupFinished);
+	}
+
+	public boolean isAvailable() {
+		return iabHelper.isBillingAvailable();
+	}
+
+	public Observable<Inventory> inventoryObservable() {
+		return inventorySubject.doOnSubscribe(this::requestInventory);
+	}
+
+	@Nullable
+	public Inventory getLatestInventory() {
+		return inventorySubject.getValue();
+	}
+
+	/* TODO: for security, generate your payload here for verification. See the comments on
+     *        verifyDeveloperPayload() for more info. Since this is a SAMPLE, we just use
+     *        an empty string, but on a production app you should carefully generate this. */
+	public void startSubscription(FragmentActivity activity, String item) {
+		startSubscription(activity, item, "");
+	}
+
+	public void startSubscription(FragmentActivity activity, String item, String payload) {
+		if (!iabHelper.isPurchaseFlowLaunched()) {
+			iabHelper.launchPurchaseFlow(activity, item, IabHelper.ITEM_TYPE_SUBS, REQUEST_PURCHASE, onPurchaseFinishedListener, payload);
+		}
+	}
+
+	public void startPurchase(FragmentActivity activity, String item) {
+		startPurchase(activity, item, "");
+	}
+
+	public void startPurchase(FragmentActivity activity, String item, String payload) {
+		if (!iabHelper.isPurchaseFlowLaunched()) {
+			iabHelper.launchPurchaseFlow(activity, item, IabHelper.ITEM_TYPE_INAPP, REQUEST_PURCHASE, onPurchaseFinishedListener, payload);
+		}
+	}
+
+	public boolean handleResult(int requestCode, int resultCode, Intent data) {
+		return iabHelper.handleActivityResult(requestCode, resultCode, data);
+	}
+
+	public void requestInventory() {
+		if (initialized && (lastInventoryJob == null || lastInventoryJob.isDone())) {
+			lastInventoryJob = background.submit(() -> {
+				try {
+					Inventory i = iabHelper.queryInventory(false, null);
+					inventorySubject.onNext(i);
+				} catch (Exception e) {
+					inventorySubject.onError(e);
+				}
+			});
+		}
+	}
+
+	@VisibleForTesting
+	void onSetupFinished(IabResult result) {
+		if (result.isSuccess()) {
+			initialized = true;
+			requestInventory();
+		} else {
+			inventorySubject.onError(new IabException(result.getResponse(), result.getMessage()));
+		}
+	}
+
+	private final IabHelper.OnIabPurchaseFinishedListener onPurchaseFinishedListener = (result, info) -> {
+		if (result.isSuccess()) {
+			requestInventory();
+		}
+	};
+}
---
+++ AndroidBillingV3/src/test/java/synergy/android/billing/reactive/ReactiveBillingTest.java
@@ -0,0 +1,73 @@
+package synergy.android.billing.reactive;
+
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.MoreExecutors;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import rx.functions.Action1;
+import synergy.android.billing.v3.util.IabHelper;
+import synergy.android.billing.v3.util.IabResult;
+import synergy.android.billing.v3.util.Inventory;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyBoolean;
+import static org.mockito.Matchers.notNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ReactiveBillingTest {
+
+	@Mock
+	IabHelper helper;
+
+	private ListeningExecutorService sameThreadExecutor = MoreExecutors.newDirectExecutorService();
+
+	@Test
+	public void when_create_then_startBillingInitialization() throws Exception {
+		new Billing(helper, sameThreadExecutor);
+
+		verify(helper).startSetup(notNull(IabHelper.OnIabSetupFinishedListener.class));
+	}
+
+	@Test
+	public void when_setupSucceeded_queryInventory() throws Exception {
+		Billing wrapper = new Billing(helper, sameThreadExecutor);
+		wrapper.onSetupFinished(new IabResult(IabHelper.BILLING_RESPONSE_RESULT_OK, null));
+
+		verify(helper).queryInventory(anyBoolean(), any());
+	}
+
+	@Test
+	public void when_setupFailed_neverQueryInventory() throws Exception {
+		Billing wrapper = new Billing(helper, sameThreadExecutor);
+		wrapper.onSetupFinished(new IabResult(IabHelper.BILLING_RESPONSE_RESULT_ERROR, null));
+		wrapper.inventoryObservable().doOnError(e -> { });
+
+		verify(helper, never()).queryInventory(anyBoolean(), any());
+	}
+
+	@Test
+	public void when_notSetup_doNotQueryInventory() throws Exception {
+		Billing wrapper = new Billing(helper, sameThreadExecutor);
+		wrapper.inventoryObservable().subscribe();
+
+		verify(helper, never()).queryInventory(anyBoolean(), any());
+	}
+
+	@Test
+	public void when_subscribedBeforeSetup_receiveInventory() throws Exception {
+		Billing wrapper = new Billing(helper, sameThreadExecutor);
+		Action1<Inventory> mockSubscription = mock(Action1.class);
+		wrapper.inventoryObservable().subscribe(mockSubscription);
+		wrapper.onSetupFinished(new IabResult(IabHelper.BILLING_RESPONSE_RESULT_OK, null));
+
+		verify(mockSubscription).call(any());
+	}
+
+}
